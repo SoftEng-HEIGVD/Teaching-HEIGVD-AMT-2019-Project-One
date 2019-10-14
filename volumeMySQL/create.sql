7c0748a7f71dfdccf2a68607d167d4b8a69c684f
Florian Polier <polier.florian@gmail.com>
2019-10-14 09:07:36 +0200

    the DB deploys automatically now

--- volumeMySQL/create.sql
+++ volumeMySQL/create.sql
@@ -1,1 +1,29 @@
-CREATE DATABASE dbAMTProject; 
+CREATE DATABASE AMTProjectOne;
+USE AMTProjectOne;
+
+CREATE TABLE Serie (
+    ID int NOT NULL PRIMARY KEY,    
+    Title varchar(255),
+    Producer varchar(255),
+    Synopsis varchar(255),
+    Genre varchar(255)
+);
+
+CREATE TABLE Viewer (
+    ID int NOT NULL PRIMARY KEY,    
+    Firstname varchar(255) NOT NULL,
+    Lastname varchar(255) NOT NULL,
+    Username varchar(255) NOT NULL UNIQUE,
+    Genre varchar(255),
+    Birthdate DATE
+);
+
+CREATE TABLE WatchingInfo (
+    IDSerie int NOT NULL,    
+    IDViewer int NOT NULL,
+    TimeSpent int NOT NULL,
+    BeginningDate DATE,
+    PRIMARY KEY (IDSerie, IDViewer),
+    FOREIGN KEY (IDSerie) REFERENCES Serie(ID),
+    FOREIGN KEY (IDViewer) REFERENCES Viewer(ID)
+);
