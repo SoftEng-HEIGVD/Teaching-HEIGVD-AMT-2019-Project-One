054e858ff21dab8d81334adc9d82cc2b2c714214
Adam <adam.zouari@heig-vd.ch>
2019-10-14 17:10:07 +0200

    fix dockercompose and css bug

---
+++ src/main/java/ch/heigvd/amt/projectone/presentation/ChilloutServlet.java
@@ -0,0 +1,24 @@
+package ch.heigvd.amt.projectone.presentation;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/chill")
+public class ChilloutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+
+}
