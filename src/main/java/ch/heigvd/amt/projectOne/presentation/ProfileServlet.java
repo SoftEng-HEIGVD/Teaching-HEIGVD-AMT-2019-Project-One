c1abe3ca335e23e2ef668883e0a6542b8343ab50
Caroline Monthoux <caroline.monthoux@heig-vd.ch>
2019-11-02 11:21:04 +0100

    modifies MyProfile page so it can either prints another character or the actual one

--- src/main/java/ch/heigvd/amt/projectOne/presentation/ProfileServlet.java
+++ src/main/java/ch/heigvd/amt/projectOne/presentation/ProfileServlet.java
@@ -1,24 +1,44 @@
 package ch.heigvd.amt.projectOne.presentation;
 
+import ch.heigvd.amt.projectOne.model.Character;
 import ch.heigvd.amt.projectOne.model.Membership;
+import ch.heigvd.amt.projectOne.services.dao.CharacterManagerLocal;
+import ch.heigvd.amt.projectOne.services.dao.MembershipManagerLocal;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(urlPatterns = "/profile")
+
 public class ProfileServlet extends HttpServlet {
+
+  @EJB
+  private MembershipManagerLocal membershipManager;
+  @EJB
+  private CharacterManagerLocal characterManager;
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    List<Membership> memberships = new ArrayList<>();
 
+    Character character;
+    HttpSession session = req.getSession();
 
+    if (req.getParameterMap().containsKey("id")) {
+      character = characterManager.getCharacterById(Integer.parseInt(req.getParameter("id")));
+    } else {
+      character = (Character) session.getAttribute("character");
+    }
+
+    List<Membership> memberships = membershipManager.getMembershipsByUserId(character.getId());
+
+    req.setAttribute("memberships", memberships);
     req.getRequestDispatcher("/WEB-INF/pages/profile.jsp").forward(req, resp);
   }
 
