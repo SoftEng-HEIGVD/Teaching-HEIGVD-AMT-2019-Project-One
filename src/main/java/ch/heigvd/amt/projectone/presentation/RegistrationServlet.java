b423472908779055e2376103dc6ff4de7b4cd026
Naguir <nair.alic@heig-vd.ch>
2019-11-03 17:40:36 +0100

    Datasource config dans Docker, probleme au niveau import du standalone.xml

--- src/main/java/ch/heigvd/amt/projectone/presentation/RegistrationServlet.java
+++ src/main/java/ch/heigvd/amt/projectone/presentation/RegistrationServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 @WebServlet(name = "RegistrationServlet", urlPatterns = "/registration")
 public class RegistrationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/pages/registration.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/pages/view.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
