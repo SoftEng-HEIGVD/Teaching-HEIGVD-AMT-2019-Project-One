67a772deb5abfad491c8e188a7f416a91da80e1d
Adam <adam.zouari@heig-vd.ch>
2019-11-04 18:05:42 +0100

    fix certificate missing, update to payara v5.193

--- images/mysql/data/a_chillout-schema.sql
+++ images/mysql/data/a_chillout-schema.sql
@@ -1,31 +1,33 @@
+#DROP DATABASE IF EXISTS chillout;
+
 CREATE DATABASE IF NOT EXISTS chillout;
 
 CREATE TABLE `chillout`.`Person` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `username` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `isAdmin` boolean
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `chillout`.`Client` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNIQUE NOT NULL AUTO_INCREMENT,
   foreign key (id) references Person(id)
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `chillout`.`Volunteer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNIQUE NOT NULL AUTO_INCREMENT,
   foreign key (id) references Person(id)
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `chillout`.`Supplier` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `chillout`.`Product` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `unitPrice` decimal(10,2) not null
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
