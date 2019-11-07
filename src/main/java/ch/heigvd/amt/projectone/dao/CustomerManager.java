bb2f3a0e1763f372a5742ef78e445e63c13f103d
Miguel Gouveia <miguel.lopesgouveia@heig-vd.ch>
2019-11-07 15:44:27 +0100

    Debut CustomerManager

--- src/main/java/ch/heigvd/amt/projectone/dao/CustomerManager.java
+++ src/main/java/ch/heigvd/amt/projectone/dao/CustomerManager.java
@@ -1,4 +1,54 @@
 package main.java.ch.heigvd.amt.projectone.dao;
 
-public class CustomerManager {
+import main.java.ch.heigvd.amt.projectone.model.Customer;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Stateless
+public class CustomerManager
+{
+    @Resource(lookup = "java:/jdbc/FlightCompany")
+    private DataSource dataSource;
+
+    public Customer getCustomer(long id)
+    {
+        Customer customer = null;
+        try {
+            Connection connection = dataSource.getConnection();
+
+            PreparedStatement sql = connection.prepareStatement("SELECT * FROM customer WHERE id = ?");
+            sql.setObject(1, id);
+
+            ResultSet result = sql.executeQuery();
+
+            while(result.next())
+            {
+
+                String pseudo = result.getString("customer_pseudo");
+                String firstname = result.getString("firstname");
+                String lastname = result.getString("lastname");
+                int age = result.getInt("age");
+                String passwd = result.getString("customer_pw");
+
+                customer = new Customer(id, pseudo, firstname, lastname, age, passwd);
+
+            }
+            connection.close();
+        } catch(SQLException ex){
+            ex.printStackTrace();
+        }
+
+        return customer;
+    }
+
+    public Customer Connection(String pseudo, String passwd)
+    {
+
+    }
 }
--- src/main/java/ch/heigvd/amt/projectone/model/Customer.java
+++ src/main/java/ch/heigvd/amt/projectone/model/Customer.java
@@ -1,4 +1,51 @@
 package main.java.ch.heigvd.amt.projectone.model;
 
-public class Customer {
+public class Customer
+{
+    private long customer_id;
+    private String customer_pseudo;
+    private String firstname;
+    private String lastname;
+    private int age;
+    private String customer_pw;
+
+    public Customer(long customer_id, String customer_pseudo, String firstname, String lastname, int age, String customer_pw)
+    {
+        this.customer_id = customer_id;
+        this.customer_pseudo = customer_pseudo;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.customer_pw = customer_pw;
+    }
+
+    public long getCustomer_id()
+    {
+        return customer_id;
+    }
+
+    public String getCustomer_pseudo()
+    {
+        return customer_pseudo;
+    }
+
+    public String getFirstname()
+    {
+        return firstname;
+    }
+
+    public String getLastname()
+    {
+        return lastname;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public String getCustomer_pw()
+    {
+        return customer_pw;
+    }
 }
