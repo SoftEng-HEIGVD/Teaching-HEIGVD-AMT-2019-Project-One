3e24681e24d5656d2b78e5fa275fc318dfce56db
Miguel Gouveia <miguel.lopesgouveia@heig-vd.ch>
2019-11-07 16:44:34 +0100

    Contunie sur CustomerManager

--- src/main/java/ch/heigvd/amt/projectone/dao/CustomerManager.java
+++ src/main/java/ch/heigvd/amt/projectone/dao/CustomerManager.java
@@ -22,7 +22,7 @@ public class CustomerManager
         try {
             Connection connection = dataSource.getConnection();
 
-            PreparedStatement sql = connection.prepareStatement("SELECT * FROM customer WHERE id = ?");
+            PreparedStatement sql = connection.prepareStatement("SELECT * FROM customer WHERE customer_id = ?");
             sql.setObject(1, id);
 
             ResultSet result = sql.executeQuery();
@@ -45,6 +45,67 @@ public class CustomerManager
         }
 
         return customer;
+    }
+
+    public boolean createCustomer()
+    {
+
+    }
+
+    public boolean deleteCustomer(long id)
+    {
+        boolean success = false;
+        int nbRow;
+        try{
+            Connection connection = dataSource.getConnection();
+
+            PreparedStatement sql = connection.prepareStatement("DELETE FROM customer WHERE customer_id = ?");
+
+            sql.setLong(1, id);
+            nbRow = sql.executeUpdate();
+            connection.close();
+
+            if(nbRow > 0)//if row are updated, return true
+            {
+                success = true;
+            }
+        }catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+        return success;
+
+    }
+
+    public boolean updateCustomer(Customer customer)
+    {
+        boolean success = false;
+        if(customer != null)
+        {
+            int nbRow;
+            try{
+                Connection connection = dataSource.getConnection();
+
+                PreparedStatement sql = connection.prepareStatement("UPDATE Customer SET customer_pseudo = ?, firstname = ?, lastname = ?, age = ?, customer_pw = ? WHERE customer_id = ?");
+
+                sql.setObject(1, customer.getCustomer_pseudo());
+                sql.setObject(2, customer.getFirstname());
+                sql.setObject(3, customer.getLastname());
+                sql.setObject(4, customer.getAge());
+                sql.setObject(5, customer.getCustomer_pw());
+                sql.setObject(6, customer.getCustomer_id());
+
+                nbRow = sql.executeUpdate();
+                connection.close();
+
+                if(nbRow > 0)//if row are updated, return true
+                {
+                    success = true;
+                }
+            }catch(SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return success;
     }
 
     public Customer Connection(String pseudo, String passwd)
