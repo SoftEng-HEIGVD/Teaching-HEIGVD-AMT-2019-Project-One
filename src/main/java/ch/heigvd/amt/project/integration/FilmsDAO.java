1afe6d02aa7c84258ee1119cec18e6d7e6b706fd
Loris <crullloris@gmail.com>
2019-11-09 22:25:30 +0100

    Added Director field

--- src/main/java/ch/heigvd/amt/project/integration/FilmsDAO.java
+++ src/main/java/ch/heigvd/amt/project/integration/FilmsDAO.java
@@ -29,11 +29,12 @@ public class FilmsDAO implements IFilmsDao {
         Connection connection = null;
         try {
             connection = dataSource.getConnection();
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO amt_films (ID, TITLE, RUNNING_TIME, PATH_TO_MOVIE_POSTER) VALUES (?,?,?,?)");
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO amt_films (ID, TITLE, RUNNING_TIME, PATH_TO_MOVIE_POSTER, DIRECTOR) VALUES (?,?,?,?,?)");
             statement.setString(1, String.valueOf(entity.getId()));
             statement.setString(2, entity.getTitle());
             statement.setString(3, String.valueOf(entity.getRunningTime()));
             statement.setString(4, entity.getMoviePosterPath());
+            statement.setString(5, entity.getDirector());
             statement.execute();
             return entity;
         } catch (SQLException e) {
@@ -49,7 +50,7 @@ public class FilmsDAO implements IFilmsDao {
         Connection connection = null;
         try {
             connection = dataSource.getConnection();
-            PreparedStatement statement = connection.prepareStatement("SELECT ID, TITLE, RUNNING_TIME, PATH_TO_MOVIE_POSTER FROM amt_films WHERE ID = ?");
+            PreparedStatement statement = connection.prepareStatement("SELECT ID, TITLE, RUNNING_TIME, PATH_TO_MOVIE_POSTER, DIRECTOR FROM amt_films WHERE ID = ?");
             statement.setString(1, id);
             ResultSet rs = statement.executeQuery();
             boolean hasRecord = rs.next();
@@ -61,6 +62,7 @@ public class FilmsDAO implements IFilmsDao {
                     .title(rs.getString(2))
                     .runningTime(Integer.parseInt(rs.getString(3)))
                     .moviePosterPath(rs.getString(4))
+                    .director(rs.getString(5))
                     .build();
             return existingFilm;
         } catch (SQLException e) {
@@ -76,11 +78,12 @@ public class FilmsDAO implements IFilmsDao {
         Connection connection = null;
         try {
             connection = dataSource.getConnection();
-            PreparedStatement statement = connection.prepareStatement("UPDATE amt_films SET TITLE=?, RUNNING_TIME=?, PATH_TO_MOVIE_POSTER=? WHERE ID = ?");
+            PreparedStatement statement = connection.prepareStatement("UPDATE amt_films SET TITLE=?, RUNNING_TIME=?, PATH_TO_MOVIE_POSTER=?, DIRECTOR=? WHERE ID = ?");
             statement.setString(1, entity.getTitle());
             statement.setString(2, String.valueOf(entity.getRunningTime()));
             statement.setString(3, entity.getMoviePosterPath());
-            statement.setString(4, String.valueOf(entity.getId()));
+            statement.setString(4, entity.getDirector());
+            statement.setString(5, String.valueOf(entity.getId()));
             int updatedFilms = statement.executeUpdate();
             if(updatedFilms != 1) {
                 throw new KeyNotFoundException("Could not find film with film_id = " + entity.getId());
@@ -129,11 +132,11 @@ public class FilmsDAO implements IFilmsDao {
                         .title(rs.getString("TITLE"))
                         .runningTime(Integer.parseInt(rs.getString("RUNNING_TIME")))
                         .moviePosterPath(rs.getString("PATH_TO_MOVIE_POSTER"))
+                        .director(rs.getString("DIRECTOR"))
                         .build();
                 existingFilms.add(existingFilm);
             }
-            return  existingFilms;
-
+            return existingFilms;
         } catch (SQLException e) {
             e.printStackTrace();
             throw new Error(e);
