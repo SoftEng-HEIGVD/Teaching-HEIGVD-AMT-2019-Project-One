4bb45780cb38de9f19052e0b1fbe0988ca89f7e2
Stephane Selim <stephan.selim@epfl.ch>
2019-11-09 16:52:25 +0100

    setup FilmViewServlet

--- src/main/java/ch/heigvd/amt/project/presentation/ServletFilmView.java
+++ src/main/java/ch/heigvd/amt/project/presentation/ServletFilmView.java
@@ -1,32 +1,19 @@
 package ch.heigvd.amt.project.presentation;
 
-import ch.heigvd.amt.project.business.FilmsManagerLocal;
-import ch.heigvd.amt.project.model.Film;
-
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-
-//TODO: Page navigation
-@WebServlet(name = "ServletHome", urlPatterns = "/home")
-public class ServletHome extends HttpServlet {
-
-    @EJB
-    FilmsManagerLocal filmsManager;
-
+@WebServlet(name = "ServletFilmView")
+public class ServletFilmView extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Film> films = filmsManager.getAllFilms();
-        request.setAttribute("films", films);
-        request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
+
     }
 }
